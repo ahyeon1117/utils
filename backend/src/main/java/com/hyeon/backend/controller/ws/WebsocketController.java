4018f8b57b20db6ab02024f940a4cdc6ccb6bc23
ahyeon1117 <ahyeon1117@naver.com>
2024-02-19 10:51:30 +0900

    [fix] 사용하지 않는 의존성 제거

--- backend/src/main/java/com/hyeon/backend/controller/ws/WebsocketController.java
+++ backend/src/main/java/com/hyeon/backend/controller/ws/WebsocketController.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
